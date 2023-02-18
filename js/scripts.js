65a0fc049e9a57a6f46321799585dbadfe862757
PavelRoR <pavelvp2182@yandex.ru>
2023-02-18 19:28:31 +0500

    Main page is stylezed

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,142 @@
 $(document).ready(function() {
-     // модалка
-    //  $('.button-up').fancybox();
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this);
+            if (!email.val().match(reNone)) {
+                email.css('borderColor','red');
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('↙ Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+
+                // button.text('Отправляем...');
+                // setTimeout(function () {
+                //     button.text('Отправлено!');
+                // }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            email.css('borderColor','#000');
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#4E4C4C",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#4E4C4C",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
+    // Модалка
+    $('.button-up').magnificPopup({
+        type: 'inline',
+        midClick: true,
+        mainClass: 'mfp-fade'
+    });
     //  Видео
     $(".video-wrapper-video .play-button").click(function () {
         var a = $(this).parent().attr("data-youtube");
         $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
     });
+     // слайдер с видео-отзывами
+     $('.revs-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '10px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        // appendArrows: '.video-revs-arrows-1',
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                centerMode: false,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true
+            }
+        }
+    ]
+    });
+    // слайдер с текстовыми отзывами
+    $('.revs-slider-text').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '10px',
+        adaptiveHeight: true,
+        centerMode: true,
+        // appendArrows: '.video-revs-arrows-2',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+    $('.slick-dots').wrap('<div class="container container-revs">')
+    // $(".video-wrapper-video .play-button").click(function () {
+    //     var a = $(this).parent().attr("data-youtube");
+    //     $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
+    // });
+    function imToVideo() {
+        $('.video-wrapper-revs iframe').each(function () {
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+        });
+        $(".video-wrapper-revs img, .play-button-rev").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
+        });
+    }
+    $('.revs-slider').on('swipe', function (event, slick, direction) {
+        imToVideo();
+    });
+    $('.revs-slider').on('afterChange', function (event, slick, direction) {
+        imToVideo();
+    });
+    $('.revs-slider').on('beforeChange', function (event, slick, direction) {
+        imToVideo();
+    });
+    /* Видео */
+    $(".video-wrapper-revs img, .play-button-rev").click(imToVideo());
 /*Конец документа*/
 });
